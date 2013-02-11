a408bae22fe3e41b422b92bd38988cafff014810
Pan <ddd@ddd.com>
2013-02-11 15:40:11 +0700

    11/02/56 15:40

--- CAT_Element/Form1.Designer.cs
+++ CAT_Element/Form1.Designer.cs
@@ -30,21 +30,25 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 67);
+            this.label1.Location = new System.Drawing.Point(23, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
@@ -53,49 +57,31 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(376, 66);
+            this.label2.Location = new System.Drawing.Point(376, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Old file :";
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(38, 109);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(315, 419);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            // 
             // textBox1
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(77, 63);
+            this.textBox1.Location = new System.Drawing.Point(77, 44);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(196, 20);
             this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "D:\\CAT_work\\MappingTest.xlsx";
+            this.textBox1.Text = "Mapping file";
             // 
             // textBox2
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(427, 63);
+            this.textBox2.Location = new System.Drawing.Point(427, 44);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(218, 20);
             this.textBox2.TabIndex = 4;
-            this.textBox2.Text = "D:\\CAT_work\\Oldtest.xlsx";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(389, 109);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(331, 419);
-            this.richTextBox2.TabIndex = 5;
-            this.richTextBox2.Text = "";
+            this.textBox2.Text = "Source file";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(278, 60);
+            this.button1.Location = new System.Drawing.Point(278, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -105,7 +91,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(651, 62);
+            this.button2.Location = new System.Drawing.Point(651, 43);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
@@ -115,15 +101,15 @@
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(761, 109);
+            this.richTextBox3.Location = new System.Drawing.Point(77, 173);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(307, 419);
+            this.richTextBox3.Size = new System.Drawing.Size(710, 388);
             this.richTextBox3.TabIndex = 8;
             this.richTextBox3.Text = "";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(519, 553);
+            this.button3.Location = new System.Drawing.Point(489, 126);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 9;
@@ -134,26 +120,80 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(874, 70);
+            this.label3.Location = new System.Drawing.Point(23, 373);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Log";
             // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(161, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(507, 77);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(34, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(378, 127);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Enter number of row to compare";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Mapping row :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(432, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Source Row";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1102, 617);
+            this.ClientSize = new System.Drawing.Size(822, 595);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -167,15 +207,19 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- CAT_Element/Form1.cs
+++ CAT_Element/Form1.cs
@@ -14,6 +14,8 @@ namespace CAT_Element
     {
         Microsoft.Office.Interop.Excel.Application m_app;
 
+        
+
         public Form1()
         {
             
@@ -54,6 +56,11 @@ namespace CAT_Element
            
             //string oldval = " ";
             //string mapval = " ";
+            string add_row = textBox8.Text;
+
+            //int aaa = Int16.Parse(textBox8.Text);
+            
+
             Workbook Mapping_workbook = m_app.Workbooks.Open(textBox1.Text,
                 Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing
                 , Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
@@ -71,14 +78,15 @@ namespace CAT_Element
             Worksheet new_sheetbook = Old_workbook.Sheets[Old_workbook.Sheets.Count];
 
             richTextBox3.Text += "Creating New sheet .....\n";
-            for (int createsheet_i = 1; createsheet_i <= 100; createsheet_i++)    //สร้าง ชีท ใหม่ โดยใช้ข้อมู,จาก old sheet
+            for (int createsheet_i = 1; createsheet_i <= Int16.Parse(textBox8.Text); createsheet_i++)    //สร้าง ชีท ใหม่ โดยใช้ข้อมู,จาก old sheet
             {
                 Range old_valuerow = old.Cells[createsheet_i, 5];
 
                 for (int createsheet = 1; createsheet <= 10; createsheet++)
                 {
                     Range old_value = old.Cells[createsheet_i, createsheet];    //ได้ ค่าจาก ชีท เดิม มา
-                    new_sheetbook.Cells[createsheet_i, createsheet] = old_value.Value;
+                    string newsheet_val = " "+old_value.Value;
+                    new_sheetbook.Cells[createsheet_i, createsheet] = newsheet_val;
                     richTextBox3.Text = "created at row [" + createsheet_i + "," + createsheet + "].....\n";
                 }
 
@@ -86,21 +94,22 @@ namespace CAT_Element
             }
             richTextBox3.Text += "New sheet are created .....\n";
 
-            string countlog = " ";
-            for (int i = 1; i <= 400; i++)
+           // string countlog = " ";
+            for (int i = 1; i <= Int16.Parse(textBox8.Text); i++)
             {
-                Range mapping_cell = mapping.Cells[i, 1];   //เรียกค่า ใน mapping sheet แถวที่ i คอลัมที่ 1
-                richTextBox1.Text = "Compare at row " + i+"\n";
-                string mapping2 = mapping_cell.Value;
+                Range mapping_cell = mapping.Cells[i, 1];   //เรียกค่า ใน mapping sheet แถวที่ i คอลัมที่ 1    
+                textBox4.Text = i.ToString();
+                string mapping2 = " "+mapping_cell.Value;
                 mapping2 = mapping2.Trim();
                 mapping2 = " " + mapping2;
 
-                for (int x = 1; x <= 10; x++) {        //กำหนดจำนวนของ แถวที่ให้ค้น หาใน sheet old
-                    richTextBox2.Text = "compare at row " + x + "\n";
+                for (int x = 1; x <= Int16.Parse(textBox8.Text); x++)
+                {        //กำหนดจำนวนของ แถวที่ให้ค้น หาใน sheet old
+                    textBox6.Text = x.ToString();
                     //richTextBox3.Text += countlog;
 
-                    Range old_cell = old.Cells[i,5];
-                    string old2 = old_cell.Value;
+                    Range old_cell = old.Cells[x,5];
+                    string old2 = " "+old_cell.Value;
 
                     old2 = old2.Trim();
                     old2 = " " + old2;
@@ -111,9 +120,10 @@ namespace CAT_Element
                     
                     if (old2 == mapping2)
                     {
-                        new_sheetbook.Cells[i, 5] = mapping.Cells[i, 6];
-                        new_sheetbook.Cells[i, 6] = mapping.Cells[i, 8];
-                      // countlog += "Edit at Row " + i + "Ma pping = " + mapping_cell.Value + ", Source file = " + old_cell.Value + " .\n";
+                        new_sheetbook.Cells[x, 5] = mapping.Cells[i, 6];
+                        new_sheetbook.Cells[x, 6] = mapping.Cells[i, 8];
+                      //countlog += "Edit at source row : "+x+" , Repalce value "+mapping.Cells[i,6]+" , "+mapping.Cells[i,8]+ ".\n";
+                        richTextBox3.Text += "\n Edit at Mapping row =" + i + " , Edit at Source file row =" + x;
                     }
                     old2 = old_cell.Value;
                    
@@ -122,9 +132,14 @@ namespace CAT_Element
                 }
                 
             }
-            richTextBox2.Text += "Compare Finished";
-            richTextBox1.Text += "Compare Finished";
-
+            richTextBox3.Text += "\n\n Compare Finished";
+            //richTextBox1.Text += "Compare Finished";
+            Old_workbook.Save();
+            m_app.Workbooks.Close();
+            m_app.Quit();
+            
         }
+
+       
     }
 }
